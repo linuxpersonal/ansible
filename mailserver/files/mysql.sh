1919707e96caed225b09e7c213ecdcd5832ec896
root <root@ansible.skull.local>
2017-10-19 15:57:39 +1100

    Update

--- mailserver/files/mysql.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mysql -e "UPDATE mysql.user SET Password = PASSWORD('mysqlpass123!!') WHERE User = 'root'"
-mysql -e "DROP USER ''@'localhost'"
-mysql -e "DROP USER ''@'$(hostname)'"
-mysql -e "DROP DATABASE test"
-mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
-mysql -e "FLUSH PRIVILEGES"
--- mailserver/files/postfix.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mysql -e "CREATE DATABASE IF NOT EXISTS postfixadmin"
-mysql -e "GRANT ALL PRIVILEGES ON postfixadmin.* TO 'postfixadmin'@'localhost' IDENTIFIED BY 'mysqlpass123!!'"
-mysql -e "FLUSH PRIVILEGES"
